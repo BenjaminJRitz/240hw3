5ad3c02e2ae3a18f43fa429cb99f549c1a8b8a26
BennyRiot <benjaminjritz@gmail.com>
2019-03-11 00:55:37 -0400

    more work

--- gradebook.cc
+++ gradebook.cc
@@ -14,3 +14,19 @@ double GradeBook::Get(size_t index) const {
     return ret;
 } // ending bracket for method Get
 
+const GradeBook GradeBook::Add(const GradeBook& that) const {
+    GradeBook newBook;
+    // loop for adding the calling gradebooks grades to the new gradebook
+    for (int i = 0; i < book.size(); ++i) {
+        newBook.Add(book[i]);
+    } // ending bracket for for loop
+    // loop for adding the parameters grades to the new gradebook
+    for (int i = 0; i < that.GetSize(); ++i) {
+        newBook.Add(that.Get(i));
+    } // ending bracket for for loop
+    return newBook;
+} // ending bracket for method Add (two books)
+
+
+
+
